aefac9efc604b5160f68207e79f8217954526e11
Maya Sheth <shethm@sh03-04n24.int>
2021-12-07 08:44:44 -0800

    call rules in stages

--- workflow/run.sh
+++ workflow/run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-CODEDIR=/oak/stanford/groups/engreitz/Users/sheth/eQTLEnrichment
+CODEDIR=/oak/stanford/groups/engreitz/Users/sheth/eQTLEnrichment-byBiosample/eQTLEnrichment
 
-snakemake -s "$CODEDIR/workflow/eQTLPredictions.snakefile" --configfile "$CODEDIR/config/config.yml" --directory $CODEDIR/ --unlock
+snakemake -s "$CODEDIR/workflow/Snakefile.snakefile" --configfile "$CODEDIR/config/config-hg38-v2.yml" --directory $CODEDIR/workflow --unlock
 
-snakemake -s "$CODEDIR/workflow/eQTLPredictions.snakefile" --configfile  "$CODEDIR/config/config.yml" -j1 --use-conda --keep-target-files --rerun-incomplete
+snakemake -s "$CODEDIR/workflow/Snakefile.snakefile" --configfile "$CODEDIR/config/config-hg38-v2.yml" -j4 --use-conda --keep-target-files --rerun-incomplete --conda-frontend conda -k --conda-prefix "$CODEDIR/workflow/envs/" -R first
 
+snakemake -s "$CODEDIR/workflow/Snakefile.snakefile" --configfile "$CODEDIR/config/config-hg38-v2.yml" -j1 --use-conda --keep-target-files --rerun-incomplete --conda-frontend conda -k --conda-prefix "$CODEDIR/workflow/envs/" -R second
+
+snakemake -s "$CODEDIR/workflow/Snakefile.snakefile" --configfile "$CODEDIR/config/config-hg38-v2.yml" -j1 --use-conda --keep-target-files --rerun-incomplete --conda-frontend conda -k --conda-prefix "$CODEDIR/workflow/envs/" -R third
+
+snakemake -s "$CODEDIR/workflow/Snakefile.snakefile" --configfile "$CODEDIR/config/config-hg38-v2.yml" -j1 --use-conda --keep-target-files --rerun-incomplete --conda-frontend conda -k --conda-prefix "$CODEDIR/workflow/envs/" -R fourth

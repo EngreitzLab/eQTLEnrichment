fc4f633c2d9702c4b177395738d4a3bd11af8c33
Maya Sheth <shethm@sh03-ln07.stanford.edu>
2022-01-12 18:37:48 -0800

    fix thresholding function

--- workflow/scripts/threshold_predictions.R
+++ workflow/scripts/threshold_predictions.R
@@ -9,16 +9,18 @@ main <- function() {
   outFile = {snakemake@output$predictionsThresholded}
   method = {snakemake@wildcards$method}
   threshold = {snakemake@wildcards$threshold} %>% as.numeric()
-  
+
   # read in prediction file
   pred = read.table(file=predFile, sep='\t', header=FALSE) %>% 
     setNames(c("chr","start","end","CellType","TargetGene","Score"))
+  pred$Score = as.numeric(pred$Score)
  
   if (method=="dist_to_tss" || method=="dist_to_gene"){ # methods where high score is worse
     pred.filtered = filter(pred, Score<threshold)
   } else { # all other methods where high score is better
-    pred.filtered = filter(pred, Score==threshold || Score>threshold)
+    pred.filtered = filter(pred, Score>=threshold)
   }
+  print(paste0("Threshold: ", threshold, ", Num pred: ", nrow(pred.filtered)))
   
   write.table(pred.filtered, file=outFile, col.names=FALSE, sep='\t', quote=FALSE, row.names=FALSE)
 }
